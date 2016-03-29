5a0f51e3b9910995262d6f399378415fa98c6c60
王俊仁 <jr-wong@qq.com>
2016-03-29 22:33:27 +0800

    搭建项目中，本地化储存使用CoreData

---
+++ NoticeMe/classes/DAO/CoreDataDAO.swift
@@ -0,0 +1,71 @@
+//
+//  CoreDataDAO.swift
+//  NoticeMe
+//
+//  Created by 王俊仁 on 16/3/29.
+//  Copyright © 2016年 王俊仁. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+class CoreDataDAO {
+
+    private init(){}
+
+    static let shareInstance = CoreDataDAO()
+
+    lazy var applicationDocumentsDirectory: NSURL = {
+        let urls = NSFileManager.defaultManager().URLsForDirectory(.DocumentDirectory, inDomains: .UserDomainMask)
+        return urls[urls.count-1]
+    }()
+
+    lazy var managedObjectModel: NSManagedObjectModel = {
+        let modelURL = NSBundle.mainBundle().URLForResource("NoticeMe", withExtension: "momd")!
+        return NSManagedObjectModel(contentsOfURL: modelURL)!
+    }()
+
+    lazy var persistentStoreCoordinator: NSPersistentStoreCoordinator = {
+
+        let coordinator = NSPersistentStoreCoordinator(managedObjectModel: self.managedObjectModel)
+        let url = self.applicationDocumentsDirectory.URLByAppendingPathComponent("SingleViewCoreData.sqlite")
+
+        var failureReason = "There was an error creating or loading the application's saved data."
+
+        do {
+            try coordinator.addPersistentStoreWithType(NSSQLiteStoreType, configuration: nil, URL: url, options: nil)
+        } catch {
+            var dict = [String: AnyObject]()
+            dict[NSLocalizedDescriptionKey] = "Failed to initialize the application's saved data"
+            dict[NSLocalizedFailureReasonErrorKey] = failureReason
+
+            dict[NSUnderlyingErrorKey] = error as NSError
+            let wrappedError = NSError(domain: "YOUR_ERROR_DOMAIN", code: 9999, userInfo: dict)
+            NSLog("Unresolved error \(wrappedError), \(wrappedError.userInfo)")
+            abort()
+        }
+
+        return coordinator
+    }()
+
+    private(set) lazy var context: NSManagedObjectContext = {
+        let coordinator = self.persistentStoreCoordinator
+        var managedObjectContext = NSManagedObjectContext(concurrencyType: .MainQueueConcurrencyType)
+        managedObjectContext.persistentStoreCoordinator = coordinator
+        return managedObjectContext
+    }()
+
+    func saveContext() {
+        if context.hasChanges {
+            do {
+                try context.save()
+            } catch {
+                let nserror = error as NSError
+                NSLog("Unresolved error \(nserror), \(nserror.userInfo)")
+                abort()
+            }
+        }
+    }
+
+
+}
--- NoticeMeTests/NoticeMeTests.swift
+++ NoticeMeTests/NoticeMeTests.swift
@@ -7,6 +7,7 @@
 //
 
 import XCTest
+import CoreData
 @testable import NoticeMe
 
 class NoticeMeTests: XCTestCase {
@@ -22,10 +23,69 @@ class NoticeMeTests: XCTestCase {
     }
     
     func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        let p = Person.newInstanceWith(CoreDataDAO.shareInstance.context, name: Person.self.description()) as? Person
+
+        p?.name = "11"
+        p?.age = 4
+
+        CoreDataDAO.shareInstance.saveContext()
+
+        let req = NSFetchRequest(entityName: Person.self.description())
+
+        do {
+            let arr: Array<Person> = try CoreDataDAO.shareInstance.context.executeFetchRequest(req) as! [Person]
+            arr.forEach { (p: Person) -> () in
+                print(p)
+            }
+        } catch {
+        }
+
     }
-    
+
+    func testGet() {
+        let req = NSFetchRequest(entityName: Person.self.description())
+
+
+        let pre = NSPredicate(format: "age = %d", 4)
+
+        req.predicate = pre
+
+        do {
+            let arr: Array<Person> = try CoreDataDAO.shareInstance.context.executeFetchRequest(req) as! [Person]
+
+            print("\(arr.count)--------------")
+
+
+        } catch {
+
+        }
+
+    }
+
+    func testDelete() {
+        let req = NSFetchRequest(entityName: Person.self.description())
+
+
+        let pre = NSPredicate(format: "age = %d", 2)
+
+        req.predicate = pre
+
+        do {
+            let arr: Array<Person> = try CoreDataDAO.shareInstance.context.executeFetchRequest(req) as! [Person]
+
+            print("\(arr.count)--------------")
+
+            arr.forEach({ (p:Person) -> () in
+                CoreDataDAO.shareInstance.context.deleteObject(p)
+            })
+
+            CoreDataDAO.shareInstance.saveContext()
+            
+        } catch {
+            
+        }
+    }
+
     func testPerformanceExample() {
         // This is an example of a performance test case.
         self.measureBlock {
